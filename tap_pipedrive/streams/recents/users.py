f337702370aad18cf2ece139f14d4c8bb0d15f95
Nick Martin <nickleomartin@gmail.com>
2020-05-15 14:01:53 +0200

    Add key properties to 'recent user' stream

--- tap_pipedrive/streams/recents/users.py
+++ tap_pipedrive/streams/recents/users.py
@@ -4,8 +4,12 @@ from tap_pipedrive.streams.recents import RecentsStream
 class RecentUsersStream(RecentsStream):
     items = 'user'
     schema = 'users'
-    key_properties = ['id', ]
-    # temporary disabled due current Pipedrive API limitations
+    key_properties = ['id',  'name', 'default_currency',
+                      'locale', 'email', 'phone', 'created',
+                      'modified', 'lang', 'active_flag', 'is_admin',
+                      'signup_flow_variation', 'has_created_company',
+                      'role_id', 'activated', 'timezone_name',
+                      'timezone_offset', 'icon_url', 'is_you']
     # state_field = 'modified'
 
     def process_row(self, row):
